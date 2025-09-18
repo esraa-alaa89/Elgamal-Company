253c027a50b1eb3218cd67f6e72a24f167529b0a
Esraa Alaa <114957507+esraa-alaa89@users.noreply.github.com>
2025-09-18 09:41:26 +0300

    Delete js directory

--- js/pages/notfound.js
+++
@@ -1,4 +0,0 @@
-
-export default function renderNotFoundPage(container) {
-    container.innerHtml= `<h1 class="text-danger text-center">NOT FOUND</h1>`
-}
--- js/utils/index.js
+++
@@ -1,8 +0,0 @@
-
-const linksList= document.querySelectorAll('a[class="nav-link"]');
-
-linksList.forEach(link => {
-    link.addEventListener('click', ()=>window.scrollTo(0,0));
-});
-
-
